38f28a367badeb5db76567d243398e12c76e2b69
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-05-15 00:51:42 +0700

    perbaikan logo

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -82,7 +82,7 @@ export function Sidebar({ className, onClose }: { className?: string, onClose?: 
             alt="SmartFarm Logo"
             width={200}
             height={80}
-            className="h-10 w-auto object-contain"
+            className="h-12 md:h-32 w-auto object-contain transition-all"
           />
         </Link>
         {onClose && (
